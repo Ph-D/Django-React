c65f97dc23308a44547e64a29a07ee5801a97920
Philippe <phildoreau45@gmail.com>
2019-02-03 22:47:27 +0100

    auth/lead_owner

---
+++ leadmanager/frontend/src/actions/leads.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { createMessage, returnErrors } from "./messages";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from "./types";
+
+// GET LEADS
+export const getLeads = () => dispatch => {
+  axios
+    .get("/api/leads/")
+    .then(res => {
+      dispatch({
+        type: GET_LEADS,
+        payload: res.data
+      });
+    })
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
+};
+
+// DELETE LEAD
+export const deleteLead = id => dispatch => {
+  axios
+    .delete(`/api/leads/${id}/`)
+    .then(res => {
+      dispatch(createMessage({ deleteLead: "Lead deleted" }));
+      dispatch({
+        type: DELETE_LEAD,
+        payload: id
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+// ADD LEAD
+export const addLead = lead => dispatch => {
+  axios
+    .post("/api/leads/", lead)
+    .then(res => {
+      dispatch(createMessage({ addLead: "Lead added" }));
+      dispatch({
+        type: ADD_LEAD,
+        payload: res.data
+      });
+    })
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
+};
